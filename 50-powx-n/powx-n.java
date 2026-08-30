e9ce234f6c488c7c47550b8e3ddf6203a6687998
Anubhab <150360566+Anubhab38@users.noreply.github.com>
2026-08-30 10:06:17 +0530

    Time: 2078 ms (1.11%) | Memory: 48.3 MB (11.10%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,29 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(x==1){
+            return 1;
+        }
+        if(x==-1 && n==-2147483648){ 
+            return 1;
+        }
+        if(n==Integer.MIN_VALUE){
+            return 0;
+        }
+        if(x==-1 && n%2!=0){
+            return -1;
+        }
+        double y = x;
+        int power=Math.abs(n);
+        for (int i = 1; i < power; i++) {
+            x = x * y;
+        }
+
+        if(n<0){
+            x=1.0/x;
+        }
+        else if(n==0){
+            x=1;
+        }
+        return x;
+    }
+}
