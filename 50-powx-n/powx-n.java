fb55e58ec094b4bbe9dbfb950c3f051993cdddc0
Anubhab <150360566+Anubhab38@users.noreply.github.com>
2026-09-05 18:14:23 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (33.98%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,30 +1,21 @@
 class Solution {
-    //SORRY FOR SUBMITTING THIS I WAS IN A VERY BAD MOOD :( :( :( :( :(
     public double myPow(double x, int n) {
-        if(x==1){
+        return bin_exp(x, (long) n);
+    }
+    public double bin_exp(double x, long n){
+        if(n==0){
             return 1;
         }
-        if(x==-1 && n==-2147483648){ 
-            return 1;
-        }
-        if(n==Integer.MIN_VALUE){
-            return 0;
-        }
-        if(x==-1 && n%2!=0){
-            return -1;
-        }
-        double y = x;
-        int power=Math.abs(n);
-        for (int i = 1; i < power; i++) {
-            x = x * y;
-        }
-
         if(n<0){
-            x=1.0/x;
+            return 1.0/bin_exp(x, -n);
         }
-        else if(n==0){
-            x=1;
+        else{
+            if(n%2==0){
+                return bin_exp(x*x, n/2);
+            }
+            else{
+                return x*bin_exp(x*x, (n-1)/2);
+            }
         }
-        return x;
     }
 }
