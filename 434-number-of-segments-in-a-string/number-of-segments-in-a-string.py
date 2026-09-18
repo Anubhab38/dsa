a19aaa7f5c77f24bc64718d2b06e3444bba0265b
Anubhab <150360566+Anubhab38@users.noreply.github.com>
2026-09-18 09:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.05%) - LeetSync

---
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        x=s.split()
+        ap=len(x)
+        return ap
+        
